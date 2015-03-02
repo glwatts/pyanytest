3472e4dcac3048f702bf742b07f9bdeb3a214b25
geoff <wattsgeoff@gmail.com>
2015-03-02 11:41:41 -0700

    View Update

--- pyanytest/views.py
+++ pyanytest/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    html = "<html><body>Hello World! This is my pythonanywhere test</body></html>"
+    html = "<html><body>Hello World! This is my NEW pythonanywhere test</body></html>"
     return HttpResponse(html)
